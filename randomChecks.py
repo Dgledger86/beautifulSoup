4f26f7418b3d234f32808778b531e864c8fa03f5
Dgledger86 <89226165+Dgledger86@users.noreply.github.com>
2022-06-09 23:34:57 -0700

    Delete randomChecks.py
    Accidentally added

--- randomChecks.py
+++
@@ -1,7 +0,0 @@
-import requests
-import bs4
-
-req = requests.get('https://wic.sjgov.org/WhosInCustody/InmateDetail/24286420')
-soup = bs4.BeautifulSoup(req.text, 'lxml')
-result = soup.select('inner-content')
-print(result)
